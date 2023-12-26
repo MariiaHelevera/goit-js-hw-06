60e29bef64e97c62cd702769c27d7c2a53cea2ab
MariiaHelevera <143526761+MariiaHelevera@users.noreply.github.com>
2023-12-26 18:23:11 +0100

    tasks done

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,33 @@
-function filterArray(numbers, value) {
-    const arr = [];
-    for (let element of numbers) {
-        if (element > value) {
-            arr.push(element);
-        }
+class StringBuilder {
+    #value;
+
+    constructor(initialValue) {
+        this.#value = initialValue;
     }
-    return arr;
+
+    getValue() {
+        return this.#value;
+    }
+
+    padEnd(str) {
+        this.#value += str;
+    }
+
+    padStart(str) {
+        this.#value = str + this.#value;
+    }
+
+    padBoth(str) {
+        this.padStart(str);
+        this.padEnd(str);
+    }
 }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
